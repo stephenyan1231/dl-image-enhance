bcb8a3e9a01ec92a9ca79d8a59575f17310be954
Zhicheng Yan <zhicheng.yan@live.com>
2014-10-03 06:28:30 +0800

    add context/get_context_data.sh

---
+++ data/uniform_set_100_xpro/context/get_context_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget https://dl.dropboxusercontent.com/u/44884434/dl_image_enhance/data/uniform_set_100_xpro/context/uniform_100_seg_voting_k0.10.tar.gz
+
+tar -xzvf uniform_100_seg_voting_k0.10.tar.gz
+
+wget https://dl.dropboxusercontent.com/u/44884434/dl_image_enhance/data/uniform_set_100_xpro/context/Parts_seg_voting_k0.10.tar.gz
+
+tar -xzvf Parts_seg_voting_k0.10.tar.gz
+
