7cfde25b78ecaea359718ef3c657c434796af66d
Zhicheng Yan <zyan3@illinois.edu>
2014-10-15 19:57:07 -0500

    commit test after moving files to dropbox

--- py/script_prepare_train_batches_uniform_set_100_xpro.sh
+++ py/script_prepare_train_batches_uniform_set_100_xpro.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #prepare training batches for xpro III effect on uniform_set_100 dataset
 
-
-dataDir="${PROJ_DIR}dl-image-enhance/data/uniform_set_100_xpro"
+inDataDir="${PROJ_DIR}dl-image-enhance/data/uniform_set_100"
+enhDataDir="${PROJ_DIR}dl-image-enhance/data/uniform_set_100_xpro"
 train_image_id_file="uniform_set_100_train_id.txt"
 test_image_id_file="uniform_set_100_test_id.txt"
 data_save_dir="uniform_set_100_7K_10_batch_seg_voting_k0.10"
 
-python trainPatchPreparer.py --num-proc=20 --ori-img-folder=${dataDir}/uniform_set_100_autotoned_tif --img-seg-dir=${dataDir}/uniform_set_100_autotoned_seg --sem-integral-map-dir=${dataDir}/context/Parts_seg_voting_k0.10 --semantic-map-dir=${dataDir}/context/uniform_100_seg_voting_k0.10 --train-image-id-file=${dataDir}/${train_image_id_file} --test-image-id-file=${dataDir}/${test_image_id_file}  --enh-img-folder=${dataDir}/uniform_set_100_local_tif  --data-save-path=${dataDir}/${data_save_dir} --num-batches=10 --preload-all-ori-img=1 --segment-random-sample-num=10 --do-compute-image-global-ftr=1 --do-local-context-SPM-regions=0 --do-compute-color-integral-map=0 --do-write-color-batch-files=1 --context-ftr-baseline=0
+python trainPatchPreparer.py --num-proc=20 --ori-img-folder=${inDataDir}/uniform_set_100_autotoned_tif --img-seg-dir=${inDataDir}/uniform_set_100_autotoned_seg --sem-integral-map-dir=${inDataDir}/context/uniform_set_100_parts_seg_voting_k0.10 --semantic-map-dir=${inDataDir}/context/uniform_set_100_seg_voting_response_map_k0.10 --train-image-id-file=${enhDataDir}/${train_image_id_file} --test-image-id-file=${enhDataDir}/${test_image_id_file}  --enh-img-folder=${enhDataDir}/uniform_set_100_xpro_tif  --data-save-path=${enhDataDir}/${data_save_dir} --num-batches=10 --preload-all-ori-img=1 --segment-random-sample-num=10 --do-compute-image-global-ftr=1 --do-local-context-SPM-regions=0 --do-compute-color-integral-map=0 --do-write-color-batch-files=1 --context-ftr-baseline=0
 
