02ff002b8bf2e399fee3765347b8289a2cf80a0c
Zhicheng Yan <zyan3@illinois.edu>
2015-01-07 09:47:34 -0600

    upload pre-trained NN models for 3 local effects

--- py/script_enhance_images_uniform_set_foregroundpopout.sh
+++ py/script_enhance_images_uniform_set_foregroundpopout.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dataDir="../../dl-image-enhance/data/uniform_set_foregroundpopout"
-cpDir=${dataDir}/convnet_checkpoints/ConvNet__2015-01-06_14.23.40zyan3-server2
+cpDir=${dataDir}/convnet_checkpoints/ConvNet__2015-01-06_20.11.39zyan3-server2
 cpName=3000.9
 
 python imageEnhancer.py -f ${cpDir}/${cpName} --out-img-dir=${cpDir}_summary --label-layer=reglayer_Lab --gpu=0 --super-pixel-enhance=1 --num-proc=8 --do-enhance-image=1 --transform-vis=0  
--- py/script_enhance_images_uniform_set_xpro.sh
+++ py/script_enhance_images_uniform_set_xpro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dataDir="../../dl-image-enhance/data/uniform_set_xpro"
-cpDir=${dataDir}/convnet_checkpoints/ConvNet__2014-12-07_10.10.12zyan3-server2
+cpDir=${dataDir}/convnet_checkpoints/ConvNet__2015-01-06_14.45.07zyan3-server2
 cpName=2000.9
 
 python imageEnhancer.py -f ${cpDir}/${cpName} --out-img-dir=${cpDir}_summary --label-layer=reglayer_Lab --gpu=0 --super-pixel-enhance=1 --num-proc=8 --do-enhance-image=1 --transform-vis=0
